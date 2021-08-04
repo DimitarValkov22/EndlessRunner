c2d001900c0513ffe0802e2f1ffac39581069c71
Dimitar Valkov <mitaka_1911@abv.bg>
2021-08-04 19:01:46 +0300

    4.8.2021
    1.Importing character and animations.
    2.Basic character movement

---
+++ EndlessRunner/Assets/Scripts/Environment/LevelBound.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelBound : MonoBehaviour
+{
+    public static float leftSide = -5.3f;
+    public static float rightSide = 5.2f;
+    public float internalLeft;
+    public float internalRight;
+
+
+    void Update()
+    {
+        internalLeft = leftSide;
+        internalRight = rightSide;
+    }
+}
