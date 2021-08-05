d0a51221618db9fafff4dca594e656ad84640f7d
Dimitar Valkov <mitaka_1911@abv.bg>
2021-08-05 19:24:45 +0300

    5.8.2021
    1.Importing environment assets
    2.Creating level sections and randomly generate them.

---
+++ EndlessRunner/Assets/Scripts/Environment/GenerateLevel.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateLevel : MonoBehaviour
+{
+    public GameObject[] section;
+    public int zPosition = 80;
+    public bool creatingSection = false;
+    public int sectionNumber;
+
+
+    void Update()
+    {
+        if(creatingSection == false)
+        {
+            creatingSection = true;
+            StartCoroutine(GenerateSection());
+        }
+    }
+
+    IEnumerator GenerateSection()
+    {
+        sectionNumber = Random.Range(0, 3);
+        Instantiate(section[sectionNumber], new Vector3(0, 0, zPosition), Quaternion.identity);
+        zPosition += 80;
+        yield return new WaitForSecondsRealtime(5);
+        creatingSection = false;
+    }
+}
