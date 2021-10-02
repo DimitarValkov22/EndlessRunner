ec0471c1850165ed12f7b9bc6ef62d1159b4b66a
Dimitar Valkov <mitaka_1911@abv.bg>
2021-10-02 22:09:31 +0300

    2.10.2021
    1. Coin import

---
+++ EndlessRunner/Assets/Scripts/Collectables/CoinCollect.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinCollect : MonoBehaviour
+{
+   // public AudioSource collectSound;
+
+     void OnTriggerEnter(Collider other)
+    {
+       // collectSound.Play();
+        this.gameObject.SetActive(false);
+    }
+}
---
+++ EndlessRunner/Assets/Scripts/Collectables/CoinRotate.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinRotate : MonoBehaviour
+{
+    private float rotateSpeed = 2.5f;
+    void Update()
+    {
+        transform.Rotate(0, rotateSpeed, 0, Space.World);
+    }
+}
